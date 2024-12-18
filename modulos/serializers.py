5daa8cf608b9bd1fc98550a919f5a4b67ba9d4f0
Jose Campos <j.camposs2701@gmail.com>
2024-12-17 22:36:18 -0500

    feat: Se realiza el tema de las restricciones para no poder eliminar un estudiante o profesor si tiene una nota asociada

--- modulos/serializers.py
+++ modulos/serializers.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from .models import Estudiante, Notas, Profesor
@@ -6,17 +7,33 @@ from .models import Estudiante, Notas, Profesor
 class EstudianteSerializer(serializers.ModelSerializer):
     
     class Meta:
-        model= Estudiante
-        fields='__all__'
+        model = Estudiante
+        fields = '__all__'
+
+    def delete(self, instance):
+        if instance.notas_set.exists():
+            raise serializers.ValidationError(
+                f"El estudiante '{instance.nombre}' no puede ser eliminado porque tiene una nota asociada"
+            )
+        instance.delete()
+
 
 class ProfesorSerializer(serializers.ModelSerializer):
     
     class Meta:
-        model= Profesor
-        fields='__all__'
+        model = Profesor
+        fields = '__all__'
+
+    def delete(self, instance):
+        if instance.notas_set.exists():
+            raise serializers.ValidationError(
+                f"El profesor '{instance.nombre}' no puede ser eliminado porque tiene tiene una nota asociada"
+            )
+        instance.delete()
+
 
 class NotasSerializer(serializers.ModelSerializer):
     
     class Meta:
-        model= Notas
-        fields='__all__'
+        model = Notas
+        fields = '__all__'
--- modulos/views.py
+++ modulos/views.py
@@ -1,31 +1,41 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
 from .models import Estudiante, Notas, Profesor
 from .serializers import (EstudianteSerializer, NotasSerializer,
                           ProfesorSerializer)
 
-# Create your views here.
-
 
 class EstudianteViewSet(viewsets.ModelViewSet):
-    """
-    ViewSet para manejar las operaciones CRUD de Sending_Commands.
-    """
     queryset = Estudiante.objects.all()
     serializer_class = EstudianteSerializer
-    
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.notas_set.exists():
+            return Response(
+                {"error": f"El estudiante '{instance.nombre}' no puede ser eliminado porque tiene notas asociadas."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 class ProfesorViewSet(viewsets.ModelViewSet):
-    """
-    ViewSet para manejar las operaciones CRUD de Sending_Commands.
-    """
     queryset = Profesor.objects.all()
     serializer_class = ProfesorSerializer
-    
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.notas_set.exists():
+            return Response(
+                {"error": f"El profesor '{instance.nombre}' no puede ser eliminado porque tiene notas asociadas."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 class NotasViewSet(viewsets.ModelViewSet):
-    """
-    ViewSet para manejar las operaciones CRUD de Sending_Commands.
-    """
     queryset = Notas.objects.all()
     serializer_class = NotasSerializer
-
